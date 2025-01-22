2009cfd69787aa6a600975d09a691ba363ed13fa
chambit oh <chambit.oh@gmail.com>
2025-01-22 22:52:21 +0200

    Strong component

--- Ex1002_StrongComponents(BruteForce)/Ex1002_StrongComponents(BruteForce).cpp
+++ Ex1002_StrongComponents(BruteForce)/Ex1002_StrongComponents(BruteForce).cpp
@@ -51,19 +51,31 @@ public:
 		vector<int> id(vertices.size(), -1);
 
 		// TODO: HasPath()를 이용해서 서로 강하게 연결된 요소들을 찾습니다.
+		for (int i = 0; i < vertices.size(); i++) {
+			if (id[i] < 0) {
+				id[i] = count;
+				for (int j = i + 1; j < vertices.size(); j++) {
+					if (HasPath(i, j) && HasPath(j, i)) {
+						id[j] = count;
+					}
+				}
+				count++;
+			}
+		}
+
 
 		// 결과 정리 후 출력
-		//vector<vector<int>> components(count);
-		//for (int s = 0; s < vertices.size(); s++)
-		//	components[id[s]].push_back(s);
-		//cout << count << " strong components" << endl;
-		//for (int i = 0; i < components.size(); i++)
-		//{
-		//	cout << "Strong component " << i + 1 << ": ";
-		//	for (auto v : components[i])
-		//		cout << v << " ";
-		//	cout << endl;
-		//}
+		vector<vector<int>> components(count);
+		for (int s = 0; s < vertices.size(); s++)
+			components[id[s]].push_back(s);
+		cout << count << " strong components" << endl;
+		for (int i = 0; i < components.size(); i++)
+		{
+			cout << "Strong component " << i + 1 << ": ";
+			for (auto v : components[i])
+				cout << v << " ";
+			cout << endl;
+		}
 	}
 
 private:
@@ -72,6 +84,16 @@ private:
 	bool HasPathHelper(Vertex* v, Vertex* t)
 	{
 		// TODO: DFS 방식으로 v와 t가 만날 수 있는 지를 확인합니다.
+		if (v == t) {
+			return true;
+		}
+
+		v->visited = true;
+		for (auto& w : v->out_neighbors) {
+			if (!w->visited) {
+				if (HasPathHelper(w, t)) return true;
+			}
+		}
 
 		return false;
 	}
